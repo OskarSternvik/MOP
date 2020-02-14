7edd1595ba2102169f9164b1897a7b65cd1d0e21
OskarSternvik <oskar.sternvik@gmail.com>
2020-02-14 14:11:53 +0100

    Fix all projects

--- autopong/startup.c
+++ autopong/startup.c
@@ -28,6 +28,8 @@ void delay_250ns(void){
 		*STK_CTRL = 0;
 		*STK_LOAD = ((168/4-1));
 		*STK_VAL;
+		*STK_CTRL = 5;
+		while((*STK_CTRL & 10000) == 0){}
 }
 void delay_500ns(void){
 	delay_250ns();
@@ -36,8 +38,10 @@ void delay_500ns(void){
 
 void delay_mikro(unsigned int us){
 	while(us--){
-		delay_500ns();
-		delay_500ns();
+		delay_250ns();
+		delay_250ns();
+		delay_250ns();
+		delay_250ns();
 	}
 
 }
@@ -53,15 +57,15 @@ void delay_milli(unsigned int ms){
 }
 
  #define GPIO_E_BASE 0x40021000
- #define GPIO_E_MODER    ((volatile unsigned int* )(GPIO_E_BASE+0x00))
+ #define GPIO_E_MODER    ((volatile unsigned int* )  (GPIO_E_BASE+0x00))
  #define GPIO_E_OTYPER   ((volatile unsigned short* )(GPIO_E_BASE+0x04))
- #define GPIO_E_OSPEEDR  ((volatile unsigned int* )(GPIO_E_BASE+0x08))
- #define GPIO_E_PUPDR    ((volatile unsigned int* )(GPIO_E_BASE+0x0C))
+ #define GPIO_E_OSPEEDR  ((volatile unsigned int* )  (GPIO_E_BASE+0x08))
+ #define GPIO_E_PUPDR    ((volatile unsigned int* )  (GPIO_E_BASE+0x0C))
  
- #define GPIO_E_IDR_LOW  ((volatile unsigned char* )(GPIO_E_BASE+0x10))
- #define GPIO_E_IDR_HIGH ((volatile unsigned char* )(GPIO_E_BASE+0x11))
- #define GPIO_E_ODR_LOW  ((volatile unsigned char* )(GPIO_E_BASE+0x14))
- #define GPIO_E_ODR_HIGH ((volatile unsigned char* )(GPIO_E_BASE+0x15))
+ #define GPIO_E_IDR_LOW  ((volatile unsigned char* ) (GPIO_E_BASE+0x10))
+ #define GPIO_E_IDR_HIGH ((volatile unsigned char* ) (GPIO_E_BASE+0x11))
+ #define GPIO_E_ODR_LOW  ((volatile unsigned char* ) (GPIO_E_BASE+0x14))
+ #define GPIO_E_ODR_HIGH ((volatile unsigned char* ) (GPIO_E_BASE+0x15))
  
  #define B_E 0x40
  #define B_SELECT 4
@@ -71,6 +75,13 @@ void delay_milli(unsigned int ms){
  #define B_RST 0x20
  #define B_CS2 0x10
  #define B_CS1 8
+ 
+ #define LCD_ON         0x3F
+ #define LCD_OFF        0x3E
+ #define LCD_SET_ADD    0x40
+ #define LCD_SET_PAGE   0xB8
+ #define LCD_DISP_START 0xC0
+ #define LCD_BUSY       0x80
  
  typedef unsigned char uint8_t;
  
@@ -89,7 +100,7 @@ void delay_milli(unsigned int ms){
 	 *GPIO_E_ODR_LOW = c;
  }
  
- void select_controller(uint8_t controller){
+ static void select_controller(uint8_t controller){
 	 switch(controller){
 		 case 0:
 			 graphic_ctrl_bit_clear(B_CS1|B_CS2);
@@ -99,8 +110,8 @@ void delay_milli(unsigned int ms){
 			 graphic_ctrl_bit_clear(B_CS2);
 			 break;
 		 case B_CS2:
-			 graphic_ctrl_bit_clear(B_CS1);
 			 graphic_ctrl_bit_set(B_CS2);
+			 graphic_ctrl_bit_clear(B_CS1);
 			 break;
 		 case B_CS1|B_CS2:
 			 graphic_ctrl_bit_set(B_CS1);
@@ -119,22 +130,23 @@ void delay_milli(unsigned int ms){
 	 while(1){
 		 graphic_ctrl_bit_set(B_E);
 		 delay_500ns();
+		 c = *GPIO_E_IDR_HIGH & LCD_BUSY;
 		 graphic_ctrl_bit_clear(B_E);
 		 delay_500ns();
 		 if(c == 0){
 			break;
 		 }
 	 }
-	 graphic_ctrl_bit_set(B_E);
+	 //graphic_ctrl_bit_set(B_E); //är med i boken
 	 *GPIO_E_MODER = 0x55555555;
 	 }
  
  uint8_t graphic_read(uint8_t controller){
 	 uint8_t c;
 	 graphic_ctrl_bit_clear(B_E);
+	 *GPIO_E_MODER = 0x00005555;
 	 graphic_ctrl_bit_set(B_RS);
 	 graphic_ctrl_bit_set(B_RW);
-	 *GPIO_E_MODER = 0x00005555;
 	 select_controller(controller);
 	 delay_500ns();
 	 graphic_ctrl_bit_set(B_E);
@@ -154,24 +166,24 @@ void delay_milli(unsigned int ms){
  }
  
  void graphic_write(uint8_t value, uint8_t controller){
-	 uint8_t c;
-	 *GPIO_E_IDR_HIGH = value;
+	 *GPIO_E_ODR_HIGH = value;
+	 select_controller(controller);
 	 delay_500ns();
 	 graphic_ctrl_bit_set(B_E);
 	 delay_500ns();
 	 graphic_ctrl_bit_clear(B_E);
 	 
-	 if(controller | B_CS1){
+	 if(controller & B_CS1){
 		 select_controller(B_CS1);
 		 graphic_wait_ready();
 	 }
-	 if(controller | B_CS2){
+	 if(controller & B_CS2){
 		 select_controller(B_CS2);
 		 graphic_wait_ready();
 	 }
-	 *GPIO_E_IDR_HIGH &= 0;
+	  *GPIO_E_ODR_HIGH &= 0;
 	 graphic_ctrl_bit_set(B_E);
-	 select_controller(0);
+	 select_controller(0); 
  }
  
  void graphic_write_command(uint8_t command, uint8_t controller){
@@ -195,12 +207,7 @@ void delay_milli(unsigned int ms){
 	 return graphic_read(controller);
  }
  
- #define LCD_ON         0x3F
- #define LCD_OFF        0x3E
- #define LCD_SET_ADD    0x40
- #define LCD_SET_PAGE   0xB8
- #define LCD_DISP_START 0xC0
- #define LCD_BUSY       0x80
+
  
  void init_app(){
 	 *(GPIO_E_MODER) = 0x55555555;
@@ -215,6 +222,7 @@ void delay_milli(unsigned int ms){
 	graphic_ctrl_bit_clear(B_E);
 	delay_milli(30);
 	graphic_ctrl_bit_set(B_RST);
+	delay_milli(100); //kan vara onödig
 	graphic_write_command(LCD_OFF,        B_CS1|B_CS2);
 	graphic_write_command(LCD_ON,         B_CS1|B_CS2);
 	graphic_write_command(LCD_DISP_START, B_CS1|B_CS2);
@@ -224,10 +232,12 @@ void delay_milli(unsigned int ms){
  }
  
  void graphic_clear_screen(void){
-	 for(int page = 0; page<8;page++){
+	 uint8_t page, add;
+	 
+	 for(page = 0; page<8;page++){
 		 graphic_write_command(LCD_SET_PAGE|page, B_CS1|B_CS2);
 		 graphic_write_command(LCD_SET_ADD|0, B_CS1|B_CS2);
-		 for(int add = 0; add<64; add++){
+		 for(add = 0; add<64; add++){
 			 graphic_write_data(0, B_CS1|B_CS2);
 		 }
 	 }
@@ -236,6 +246,9 @@ void delay_milli(unsigned int ms){
  void pixel(unsigned x, unsigned y, unsigned set){
 	 uint8_t mask, temp, controller;
 	 int index = ((y-1)/8);
+	 
+	 if((x < 1) || (y < 1) || (x > 128) || (y > 64)) return;
+	 
 	 switch((y-1)%8){
 		case 0:mask = 1;break;
 		case 1:mask = 2;break;
@@ -256,11 +269,11 @@ void delay_milli(unsigned int ms){
 		 controller = B_CS1;
 		 x = x-1;
 	 }
-	 graphic_write_command(LCD_SET_ADD|x, controller);
-	 graphic_write_command(LCD_SET_PAGE|index, controller);
+	 graphic_write_command(LCD_SET_ADD | x, controller);
+	 graphic_write_command(LCD_SET_PAGE | index, controller);
 	 temp = graphic_read_data(controller);
 	 graphic_write_command(LCD_SET_ADD|x, controller);
-	 if(set == 0){
+	 if(set){
 		 mask |= temp;
 	 } else{
 		 mask &= temp;
--- graphicdisplay/startup.c
+++ graphicdisplay/startup.c
@@ -3,14 +3,6 @@
  *
  */
  
-
- 
- #define STK_CTRL ((volatile unsigned int*)(0xE000E010))
- #define STK_LOAD ((volatile unsigned int*)(0xE000E014))
- #define STK_VAL  ((volatile unsigned int*)(0xE000E018))
- 
- #define SIMULATOR
- 
 __attribute__((naked)) __attribute__((section (".start_section")) )
 void startup ( void )
 {
@@ -20,7 +12,11 @@ __asm__ volatile(" BL main\n");					/* call main */
 __asm__ volatile(".L1: B .L1\n");				/* never return */
 }
 
-
+ #define STK_CTRL ((volatile unsigned int*)(0xE000E010))
+ #define STK_LOAD ((volatile unsigned int*)(0xE000E014))
+ #define STK_VAL  ((volatile unsigned int*)(0xE000E018))
+ 
+ #define SIMULATOR
 
 void delay_250ns(void){
 	#ifdef SIMULATOR
@@ -30,6 +26,8 @@ void delay_250ns(void){
 		*STK_CTRL = 0;
 		*STK_LOAD = ((168/4-1));
 		*STK_VAL;
+		*STK_CTRL = 5;
+		while((*STK_CTRL & 10000) == 0){}
 }
 void delay_500ns(void){
 	delay_250ns();
@@ -38,8 +36,10 @@ void delay_500ns(void){
 
 void delay_mikro(unsigned int us){
 	while(us--){
-		delay_500ns();
-		delay_500ns();
+		delay_250ns();
+		delay_250ns();
+		delay_250ns();
+		delay_250ns();
 	}
 
 }
@@ -55,15 +55,15 @@ void delay_milli(unsigned int ms){
 }
 
  #define GPIO_E_BASE 0x40021000
- #define GPIO_E_MODER    ((volatile unsigned int* )(GPIO_E_BASE+0x00))
+ #define GPIO_E_MODER    ((volatile unsigned int* )  (GPIO_E_BASE+0x00))
  #define GPIO_E_OTYPER   ((volatile unsigned short* )(GPIO_E_BASE+0x04))
- #define GPIO_E_OSPEEDR  ((volatile unsigned int* )(GPIO_E_BASE+0x08))
- #define GPIO_E_PUPDR    ((volatile unsigned int* )(GPIO_E_BASE+0x0C))
+ #define GPIO_E_OSPEEDR  ((volatile unsigned int* )  (GPIO_E_BASE+0x08))
+ #define GPIO_E_PUPDR    ((volatile unsigned int* )  (GPIO_E_BASE+0x0C))
  
- #define GPIO_E_IDR_LOW  ((volatile unsigned char* )(GPIO_E_BASE+0x10))
- #define GPIO_E_IDR_HIGH ((volatile unsigned char* )(GPIO_E_BASE+0x11))
- #define GPIO_E_ODR_LOW  ((volatile unsigned char* )(GPIO_E_BASE+0x14))
- #define GPIO_E_ODR_HIGH ((volatile unsigned char* )(GPIO_E_BASE+0x15))
+ #define GPIO_E_IDR_LOW  ((volatile unsigned char* ) (GPIO_E_BASE+0x10))
+ #define GPIO_E_IDR_HIGH ((volatile unsigned char* ) (GPIO_E_BASE+0x11))
+ #define GPIO_E_ODR_LOW  ((volatile unsigned char* ) (GPIO_E_BASE+0x14))
+ #define GPIO_E_ODR_HIGH ((volatile unsigned char* ) (GPIO_E_BASE+0x15))
  
  #define B_E 0x40
  #define B_SELECT 4
@@ -73,6 +73,13 @@ void delay_milli(unsigned int ms){
  #define B_RST 0x20
  #define B_CS2 0x10
  #define B_CS1 8
+ 
+ #define LCD_ON         0x3F
+ #define LCD_OFF        0x3E
+ #define LCD_SET_ADD    0x40
+ #define LCD_SET_PAGE   0xB8
+ #define LCD_DISP_START 0xC0
+ #define LCD_BUSY       0x80
  
  typedef unsigned char uint8_t;
  
@@ -91,7 +98,7 @@ void delay_milli(unsigned int ms){
 	 *GPIO_E_ODR_LOW = c;
  }
  
- void select_controller(uint8_t controller){
+ static void select_controller(uint8_t controller){
 	 switch(controller){
 		 case 0:
 			 graphic_ctrl_bit_clear(B_CS1|B_CS2);
@@ -101,8 +108,8 @@ void delay_milli(unsigned int ms){
 			 graphic_ctrl_bit_clear(B_CS2);
 			 break;
 		 case B_CS2:
-			 graphic_ctrl_bit_clear(B_CS1);
 			 graphic_ctrl_bit_set(B_CS2);
+			 graphic_ctrl_bit_clear(B_CS1);
 			 break;
 		 case B_CS1|B_CS2:
 			 graphic_ctrl_bit_set(B_CS1);
@@ -121,22 +128,23 @@ void delay_milli(unsigned int ms){
 	 while(1){
 		 graphic_ctrl_bit_set(B_E);
 		 delay_500ns();
+		 c = *GPIO_E_IDR_HIGH & LCD_BUSY;
 		 graphic_ctrl_bit_clear(B_E);
 		 delay_500ns();
 		 if(c == 0){
 			break;
 		 }
 	 }
-	 graphic_ctrl_bit_set(B_E);
+	 //graphic_ctrl_bit_set(B_E); //är med i boken
 	 *GPIO_E_MODER = 0x55555555;
 	 }
  
  uint8_t graphic_read(uint8_t controller){
 	 uint8_t c;
 	 graphic_ctrl_bit_clear(B_E);
+	 *GPIO_E_MODER = 0x00005555;
 	 graphic_ctrl_bit_set(B_RS);
 	 graphic_ctrl_bit_set(B_RW);
-	 *GPIO_E_MODER = 0x00005555;
 	 select_controller(controller);
 	 delay_500ns();
 	 graphic_ctrl_bit_set(B_E);
@@ -156,24 +164,24 @@ void delay_milli(unsigned int ms){
  }
  
  void graphic_write(uint8_t value, uint8_t controller){
-	 uint8_t c;
-	 *GPIO_E_IDR_HIGH = value;
+	 *GPIO_E_ODR_HIGH = value;
+	 select_controller(controller);
 	 delay_500ns();
 	 graphic_ctrl_bit_set(B_E);
 	 delay_500ns();
 	 graphic_ctrl_bit_clear(B_E);
 	 
-	 if(controller | B_CS1){
+	 if(controller & B_CS1){
 		 select_controller(B_CS1);
 		 graphic_wait_ready();
 	 }
-	 if(controller | B_CS2){
+	 if(controller & B_CS2){
 		 select_controller(B_CS2);
 		 graphic_wait_ready();
 	 }
-	 *GPIO_E_IDR_HIGH &= 0;
+	  *GPIO_E_ODR_HIGH &= 0;
 	 graphic_ctrl_bit_set(B_E);
-	 select_controller(0);
+	 select_controller(0); 
  }
  
  void graphic_write_command(uint8_t command, uint8_t controller){
@@ -197,12 +205,7 @@ void delay_milli(unsigned int ms){
 	 return graphic_read(controller);
  }
  
- #define LCD_ON         0x3F
- #define LCD_OFF        0x3E
- #define LCD_SET_ADD    0x40
- #define LCD_SET_PAGE   0xB8
- #define LCD_DISP_START 0xC0
- #define LCD_BUSY       0x80
+
  
  void init_app(){
 	 *(GPIO_E_MODER) = 0x55555555;
@@ -217,6 +220,7 @@ void delay_milli(unsigned int ms){
 	graphic_ctrl_bit_clear(B_E);
 	delay_milli(30);
 	graphic_ctrl_bit_set(B_RST);
+	delay_milli(100); //kan vara onödig
 	graphic_write_command(LCD_OFF,        B_CS1|B_CS2);
 	graphic_write_command(LCD_ON,         B_CS1|B_CS2);
 	graphic_write_command(LCD_DISP_START, B_CS1|B_CS2);
@@ -226,10 +230,12 @@ void delay_milli(unsigned int ms){
  }
  
  void graphic_clear_screen(void){
-	 for(int page = 0; page<8;page++){
+	 uint8_t page, add;
+	 
+	 for(page = 0; page<8;page++){
 		 graphic_write_command(LCD_SET_PAGE|page, B_CS1|B_CS2);
 		 graphic_write_command(LCD_SET_ADD|0, B_CS1|B_CS2);
-		 for(int add = 0; add<64; add++){
+		 for(add = 0; add<64; add++){
 			 graphic_write_data(0, B_CS1|B_CS2);
 		 }
 	 }
@@ -238,7 +244,10 @@ void delay_milli(unsigned int ms){
  void pixel(unsigned x, unsigned y, unsigned set){
 	 uint8_t mask, temp, controller;
 	 int index = ((y-1)/8);
-	 switch((y-1)%8) {
+	 
+	 if((x < 1) || (y < 1) || (x > 128) || (y > 64)) return;
+	 
+	 switch((y-1)%8){
 		case 0:mask = 1;break;
 		case 1:mask = 2;break;
 		case 2:mask = 4;break;
@@ -258,18 +267,17 @@ void delay_milli(unsigned int ms){
 		 controller = B_CS1;
 		 x = x-1;
 	 }
-	 graphic_write_command(LCD_SET_ADD|x, controller);
-	 graphic_write_command(LCD_SET_PAGE|index, controller);
+	 graphic_write_command(LCD_SET_ADD | x, controller);
+	 graphic_write_command(LCD_SET_PAGE | index, controller);
 	 temp = graphic_read_data(controller);
 	 graphic_write_command(LCD_SET_ADD|x, controller);
-	 if(set == 0){
+	 if(set){
 		 mask |= temp;
 	 } else{
 		 mask &= temp;
 	 }
 	 graphic_write_data(mask, controller);
  }
- //hello
 void main(void)
 {
 	unsigned i;
